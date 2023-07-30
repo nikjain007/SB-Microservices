5130215b35f9bd043482920b98ce2a9f2d77c6e0
nikhil.jain1 <nikhil.jain@belden.com>
2023-07-30 23:16:22 +0530

    Added further changes and distributed log tracing dependencies

---
+++ OrderService/src/main/java/com/sbpractice/OrderService/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.sbpractice.OrderService.config;
+
+import com.sbpractice.OrderService.external.decoder.CustomErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    ErrorDecoder errorDecoder(){
+        return new CustomErrorDecoder();
+    }
+}
