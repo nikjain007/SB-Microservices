696df5a0698b90526b4368d363755542e9fc1ab2
nikhil.jain1 <nikhil.jain@belden.com>
2023-08-01 18:59:43 +0530

    Added change for API gateway and circuit-breaker

---
+++ CloudGateway/src/main/java/com/sbpractice/CloudGateway/controller/FallbackController.java
@@ -0,0 +1,23 @@
+package com.sbpractice.CloudGateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/orderServiceFallBack")
+    public String orderServiceFallback(){
+        return "Order Service is down";
+    }
+
+    @GetMapping("/paymentServiceFallBack")
+    public String paymentServiceFallBack(){
+        return "Payment Service is down";
+    }
+
+    @GetMapping("/productServiceFallBack")
+    public String productServiceFallBack(){
+        return "Product Service is down";
+    }
+}
